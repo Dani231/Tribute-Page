16884686c0f4d584307f45755e82603314d62203
Daniel Balan <86018770+Dani231@users.noreply.github.com>
2021-08-06 18:10:16 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+function TributePage() {
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Aaliyah</h1>
+        <p> Tribute Page</p>
+      </div>
+      <div className="life">
+        <h3>Life</h3>
+        <p>Singer and actress Aaliyah Dana Haughton was born on January 16, 1979, in Brooklyn, New York. Raised in Detroit, Michigan, the young singer competed unsuccessfully on the television program Star Search at age 11. Later that same year, she performed with R&B legend Gladys Knight, the former wife of her uncle and manager, Barry Hankerson, at a five-night stand in Las Vegas.</p>
+        <h3>Budding Film Career</h3>
+        <p>In 2000, Aaliyah made her acting debut in the surprise action hit Romeo Must Die, starring opposite martial arts star Jet Li in a Romeo and Juliet-inspired story set in modern-day Los Angeles. She was also an executive producer of the movie's soundtrack and performed the hit single "Try Again," which netted her a second Grammy nomination as well as two MTV Music Video Awards for Best Female Video and Best Video From a Film.</p>
+      </div>
+      <div className="tribute">
+        <img src="/image/Aaliyah.jpg" className="App-logo" alt="tribute" />
+      </div>
+      <div className="death">
+        <h3>Tragic Death</h3>
+        <p>Tragically, Aaliyah was killed on August 25, 2001, when a small Cessna passenger plane carrying the singer and her video crew crashed and burst into flames shortly after takeoff from Abaco Island in the Bahamas, where they had just completed work on a video. The plane was headed for Miami, Florida. Aaliyah and seven other people, including the pilot, were believed to have died instantly, while a ninth passenger died later at a Bahamian hospital. Aaliyah was 22 years old at the time of her death. </p>
+      </div>
+      <footer>
+       <a style ={{fontSize: '20px'}} href="#">For More Info</a>
+      </footer>
+    </div>
+  );
+}
